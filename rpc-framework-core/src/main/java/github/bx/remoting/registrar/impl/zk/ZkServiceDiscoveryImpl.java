215d398557ec93c15a6122267ecdd641e5240b9c
bx <945143470@qq.com>
2021-09-04 22:48:59 +0800

    注册中心功能基本完成

--- rpc-framework-core/src/main/java/github/bx/remoting/registrar/impl/zk/ZkServiceDiscoveryImpl.java
+++ rpc-framework-core/src/main/java/github/bx/remoting/registrar/impl/zk/ZkServiceDiscoveryImpl.java
@@ -1,13 +1,26 @@
 package github.bx.remoting.registrar.impl.zk;
 
+import github.bx.exception.RpcException;
 import github.bx.remoting.dto.RpcRequest;
 import github.bx.remoting.registrar.ServiceDiscovery;
+import github.bx.remoting.registrar.impl.zk.util.CuratorUtils;
 
 import java.net.InetSocketAddress;
+import java.util.List;
 
 public class ZkServiceDiscoveryImpl implements ServiceDiscovery {
     @Override
     public InetSocketAddress lookupService(RpcRequest rpcRequest) {
-        return new InetSocketAddress("192.168.56.1", 6677);
+        String serviceName = rpcRequest.getRpcServiceName();
+        List<String> serviceUrlList = CuratorUtils.getChildrenNodes(serviceName);
+        if (serviceUrlList == null || serviceUrlList.size() == 0)
+            throw new RpcException("no such service: " + serviceName);
+        /*
+        * TODO：添加负载均衡功能，这里默认获取第一个
+        * */
+        String[] addressStr = serviceUrlList.get(0).split(":");
+        String host = addressStr[0];
+        int port = Integer.parseInt(addressStr[1]);
+        return new InetSocketAddress(host, port);
     }
 }
--- rpc-framework-core/src/main/java/github/bx/remoting/registrar/impl/zk/ZkServiceRegistrarImpl.java
+++ rpc-framework-core/src/main/java/github/bx/remoting/registrar/impl/zk/ZkServiceRegistrarImpl.java
@@ -1,12 +1,15 @@
 package github.bx.remoting.registrar.impl.zk;
 
 import github.bx.remoting.registrar.ServiceRegistrar;
+import github.bx.remoting.registrar.impl.zk.util.CuratorUtils;
+import org.apache.curator.framework.CuratorFramework;
 
 import java.net.InetSocketAddress;
 
 public class ZkServiceRegistrarImpl implements ServiceRegistrar {
     @Override
     public void registerService(String serviceName, InetSocketAddress address) {
-
+        String path = CuratorUtils.ZK_REGISTER_ROOT_PATH + "/" + serviceName + address.toString();
+        CuratorUtils.createPersistentNode(path);
     }
 }
