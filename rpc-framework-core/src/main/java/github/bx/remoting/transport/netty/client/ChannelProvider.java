c17d1f9268eed83915240d227d9bf117294f9fc1
bx <945143470@qq.com>
2021-09-16 00:05:11 +0800

    Map改为使用ConcurrentHashMap；优化getChannel逻辑

--- rpc-framework-core/src/main/java/github/bx/remoting/transport/netty/client/ChannelProvider.java
+++ rpc-framework-core/src/main/java/github/bx/remoting/transport/netty/client/ChannelProvider.java
@@ -5,17 +5,28 @@ import io.netty.channel.Channel;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ChannelProvider {
     private Map<String, Channel> channelMap;
 
     public ChannelProvider() {
-        channelMap = new HashMap<>();
+        channelMap = new ConcurrentHashMap<>();
     }
 
     public Channel getChannel(InetSocketAddress address) {
         String addrStr = address.toString();
-        return channelMap.get(addrStr);
+
+        if (channelMap.containsKey(addrStr)) {
+            Channel channel = channelMap.get(addrStr);
+            // 如果channel可用，则返回，否则移除，返回null
+            if (channel != null && channel.isActive())
+                return channel;
+            else
+                channelMap.remove(addrStr);
+        }
+
+        return null;
     }
 
     public void setChannel(InetSocketAddress inetSocketAddress, Channel channel) {
